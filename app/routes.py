4d2087c3b4ca8bd8538b494f51a9d01d9dae4e8d
john-the1-mwagiru <johnmwagiru199@gmail.com>
2025-02-19 18:00:52 +0300

    feature-7:created a view function for the categories page where active users will be able to view their saved recipe categories and also modified the log in view function

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,54 @@
-from flask import flash, request,render_template, url_for,redirect, session
+from flask import flash, request, render_template, url_for, redirect, session
 from app import app
-from app.database import users,category
+from app.database import users, category
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
-@app.route('/index')
-def index():
 
-    return  render_template("index.html")
-@app.route('/log-in', methods=['POST', 'GET'])
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/log-in", methods=["POST", "GET"])
 def log_in():
     error = None
     if request.method == "POST":
         username = request.form["username"]
-        session["username"]=username
         password = request.form["password"]
-        all =users.UserModel.get_all()
-        for user in all:
-            if user.email == username and user.password == password  :
-                flash("You were successfully logged in")
-                return redirect(url_for("index"))
-            error = "Invalid credentials"    
-            return render_template("login.html", username=username, password=password, error=error)
-    
-    return render_template("login.html")
+        session["username"] = username
+        all_users = users.UserModel.saved_users
+        for active_user in all_users:
+            if active_user.email == username and active_user.password == password:
+                return redirect(url_for("index", username=username))
+        error = "Invalid credentials"
+        return render_template(
+            "login.html", error=error, username=username, password=password
+        )
+    else:
+        return render_template("login.html")
 
 
+@app.route("/<username>")
+def user(username):
+    return f"<h1>{username}</h1>"
 
-        
+
+@app.route("/categories")
+def username():
+    if "username" in session:
+        username = session["username"]
+        all = users.UserModel.get_all()
+        categories = category.CategoryModel.get_all()
+        user = [logged_user for logged_user in all if logged_user.email == username]
+
+        for a_user in user:
+            user_category = [
+                category
+                for category in categories
+                if category.user_id == a_user.user_id
+            ]
+            return render_template("categories.html", user_category=user_category)
+    else:
+        return redirect(url_for("index"))
